615a0bd6d49362e2abe4730860bce2663b2990b8
RobertoVz <roberto@opalo.studio>
2020-03-02 17:09:52 -0600

    Listo

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,13 @@
 module.exports = {
-  transpileDependencies: ["vuetify"]
-};
+  transpileDependencies: ['vuetify'],
+
+  pwa: {
+    short_name: '<Ópalo> Studio',
+    name: '<Ópalo> Studio',
+    themeColor: '#fe694e',
+    manifestOptions: {
+      background_color: '#fe694e'
+    },
+    msTileColor: '#fe694e'
+  }
+}
