28ce241e3958b12b1dd994292d6662ffe8781c53
RobertoVz <roberto@opalo.studio>
2020-03-02 17:33:53 -0600

    Listo para empezar a maquetar

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,9 @@ import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Welcome from '../views/Welcome.vue'
 import FormClientes from '../views/FormClientes.vue'
+import PanelControl from '../views/PanelControl.vue'
+import Dashboard from '../components/Dashboard.vue'
+import RespuestasForm from '../components/RespuestasForm.vue'
 
 Vue.use(VueRouter)
 
@@ -21,6 +24,30 @@ const routes = [
     path: '/formularioClientes',
     name: 'FormularioClientes',
     component: FormClientes
+  },
+  {
+    path: '/panelControl',
+    name: 'PanelControl',
+    component: PanelControl,
+    children: [
+      {
+        path: 'dashboard',
+        name: 'Dashboard',
+        component: Dashboard
+      },
+      {
+        path: 'respuestasForm',
+        name: 'RespuestasForm',
+        component: RespuestasForm
+      },
+      {
+        path: '',
+        redirect: 'dashboard'
+      }
+    ],
+    meta: {
+      transitionName: `slide`
+    }
   }
 ]
 
